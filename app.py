95e1a1fbd7c3cd12371d8940ff32ef12b846dbac
Aarush Aggarwal <91485103+iam4tart@users.noreply.github.com>
2022-11-13 03:42:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template,request
 import pred as p
 app=Flask(__name__)
-@app.route("/hi")
+@app.route("/")
 def hello():
     return render_template("index.html")
 @app.route("/sub",methods=["POST"])
@@ -9,11 +9,17 @@ def submit():
     if request.method=="POST":
         dish=request.form["dish"]
         dish_pred=p.dish_recommender(dish)
+        most = dish_pred[0].split()
+        most_dish_1 = (" ".join(most[0:-2])).title()
+        print(most_dish_1)
+        most_dish_2 = (" ".join(most[-2:])).title()
+        print(most_dish_2)
         ing1=request.form["ing1"]
         ing2=request.form["ing2"]
         ing3=request.form["ing3"]
         dishes=p.dish_ing(ing1,ing2,ing3)
-    return render_template("sub.html",n=dish_pred,m=dishes)
+        print(dishes)
+    return render_template("sub.html",most_dish_1=most_dish_1,most_dish_2=most_dish_2,m=dishes)
 
 if __name__=="__main__":
     app.run()
