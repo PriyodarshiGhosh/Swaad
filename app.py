20303f6bcdb7b7afee6634a0faba0e661c89759e
Aarush Aggarwal <91485103+iam4tart@users.noreply.github.com>
2022-11-13 12:04:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,42 @@
-from flask import Flask,render_template,request
+# from flask import Flask,render_template,request,redirect,flash
+from flask import *
 import pred as p
 app=Flask(__name__)
 @app.route("/")
 def hello():
     return render_template("index.html")
-@app.route("/sub",methods=["POST"])
+@app.route("/sub",methods=["GET","POST"])
 def submit():
     if request.method=="POST":
         dish=request.form["dish"]
-        dish_pred=p.dish_recommender(dish)
-        most = dish_pred[0].split()
-        most_dish_1 = (" ".join(most[0:-2])).title()
-        print(most_dish_1)
-        most_dish_2 = (" ".join(most[-2:])).title()
-        print(most_dish_2)
         ing1=request.form["ing1"]
         ing2=request.form["ing2"]
         ing3=request.form["ing3"]
-        dishes=p.dish_ing(ing1,ing2,ing3)
-        print(dishes)
-    return render_template("sub.html",most_dish_1=most_dish_1,most_dish_2=most_dish_2,m=dishes)
+
+        # print("<!--",dish, ing1, ing2, ing3,"-->")
+        if (dish != "") & (ing1 == "") & (ing2 == "") & (ing3 == ""):
+            dish_pred=p.dish_recommender(dish)
+            print(dish_pred)
+            most = dish_pred[0][0]
+            most_desc = dish_pred[1][0]
+            sim_2 = dish_pred[0][1]
+            sim_2_desc = dish_pred[1][1]
+            sim_3 = dish_pred[0][2]
+            sim_3_desc = dish_pred[1][2]
+            sim_4 = dish_pred[0][3]
+            sim_4_desc = dish_pred[1][3]
+            sim_5 = dish_pred[0][4]
+            sim_5_desc = dish_pred[1][4]
+            return render_template("sub.html",most=most, sim_2=sim_2, sim_3=sim_3, sim_4=sim_4, sim_5=sim_5, most_desc=most_desc, sim_2_desc=sim_2_desc, sim_3_desc=sim_3_desc, sim_4_desc=sim_4_desc, sim_5_desc=sim_5_desc)
+        
+        elif (dish == "") & (ing1 != "") & (ing2 != "") & (ing3 != ""):
+            dishes=p.dish_ing(ing1,ing2,ing3)
+            print(dishes)
+            return render_template("sub.html",m=dishes)
+
+        else:
+            print("breaking out")
+            return redirect('/')
 
 if __name__=="__main__":
     app.run()
